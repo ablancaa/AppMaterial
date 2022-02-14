fc5420375be0ffe8660d4e4b9edf2cbff6d12234
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-14 19:49:54 +0100

    Avance en About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,79 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div class="card" style="width: 18rem;">
-  <img src="" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
- </div>
-
+<div class="container">
+  <div class="row">
+    <div class="col-12 col-md-4 col-sm-4">
+      <div class="card" style="width: 20rem;">
+        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div><!-- FIN card -->
+    </div><!-- FIN col -->
+    <div class="col-12 col-md-4 col-sm-6">
+      <div class="card" style="width: 20rem;">
+        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div><!-- FIN card -->  
+    </div><!-- FIN col -->
+    <div class="col-12 col-md-4 col-sm-6">
+      <div class="card" style="width: 20rem;">
+        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div><!-- FIN card -->  
+    </div><!-- FIN col -->
+    <div class="col-12 col-md-4 col-sm-6">
+      <div class="card" style="width: 20rem;">
+        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div><!-- FIN card -->  
+    </div><!-- FIN col -->
+    <div class="col-12 col-md-4 col-sm-6">
+      <div class="card" style="width: 20rem;">
+        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div><!-- FIN card -->  
+    </div><!-- FIN col -->
+    <div class="col-12 col-md-4 col-sm-6">
+      <div class="card" style="width: 20rem;">
+        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div><!-- FIN card -->  
+    </div><!-- FIN col -->
+  </div><!-- FIN Row -->  
+</div><!-- FIN Container -->
 </template>
+
+<script>
+  export default {
+    name: 'About'
+  }
+</script>
+
+<style>
+.card {
+  margin-top: 25px;
+  padding: 15px;
+}
+</style>
