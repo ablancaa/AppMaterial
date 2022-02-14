f93ef362cae3b186dac37d18cfef051c0615c6dd
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-14 18:09:34 +0100

    Barra de navegación

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-link to="/about">Alberto</router-link>
-    <router-link to="/about">Blanca</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/about">Alberto</router-link> |
+    <router-link to="/about">Blanca</router-link> |
   </div>
   <router-view/>
 </template>
@@ -18,7 +18,7 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 20px;
 }
 
 #nav a {
