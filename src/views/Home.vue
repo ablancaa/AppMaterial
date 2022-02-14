ed1b1551ae7775c18fc29d966a7b05c1ad14dafd
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-14 13:40:03 +0100

    inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Inicio de app para Material"/>
+    <HelloWorld msg="Inicio de app para Material "/>
   </div>
 </template>
 
