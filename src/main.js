54bb47eed210d2bb3161e51dfdd4527fd1f707a2
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-14 18:23:24 +0100

    Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
@@ -5,3 +6,5 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
