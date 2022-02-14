4f420e819ab741497ae8c4d95218dbd1ff38f514
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-02-14 20:08:47 +0100

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,65 +1,77 @@
 <template>
 <div class="container">
   <div class="row">
-    <div class="col-12 col-md-4 col-sm-4">
-      <div class="card" style="width: 20rem;">
-        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div><!-- FIN card -->
+    <div class="col-12 col-sm-6 col-lg-4">
+      <div class="container">
+        <div class="card" style="width: 20rem;">
+          <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Recogida</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div><!-- FIN card -->
+      </div><!-- FIN container -->
     </div><!-- FIN col -->
-    <div class="col-12 col-md-4 col-sm-6">
-      <div class="card" style="width: 20rem;">
-        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div><!-- FIN card -->  
+    <div class="col-12 col-sm-6 col-lg-4">
+      <div class="container">
+        <div class="card" style="width: 20rem;">
+          <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Devolución</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div><!-- FIN card -->
+      </div><!-- FIN container -->
     </div><!-- FIN col -->
-    <div class="col-12 col-md-4 col-sm-6">
-      <div class="card" style="width: 20rem;">
-        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div><!-- FIN card -->  
+    <div class="col-12 col-sm-6 col-lg-4">
+      <div class="container">
+        <div class="card" style="width: 20rem;">
+          <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Esterilización</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div><!-- FIN card -->  
+      </div><!-- FIN container -->
     </div><!-- FIN col -->
-    <div class="col-12 col-md-4 col-sm-6">
-      <div class="card" style="width: 20rem;">
-        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div><!-- FIN card -->  
+    <div class="col-12 col-sm-6 col-lg-4">
+      <div class="container">
+        <div class="card" style="width: 20rem;">
+          <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div><!-- FIN card -->  
+      </div><!-- FIN container -->
     </div><!-- FIN col -->
-    <div class="col-12 col-md-4 col-sm-6">
-      <div class="card" style="width: 20rem;">
-        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div><!-- FIN card -->  
+    <div class="col-12 col-sm-6 col-lg-4">
+      <div class="container">
+        <div class="card" style="width: 20rem;">
+          <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div><!-- FIN card -->  
+      </div><!-- FIN container -->
     </div><!-- FIN col -->
-    <div class="col-12 col-md-4 col-sm-6">
-      <div class="card" style="width: 20rem;">
-        <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div><!-- FIN card -->  
+    <div class="col-12 col-sm-6 col-lg-4">
+      <div class="container">
+        <div class="card" style="width: 20rem;">
+          <img src="https://picsum.photos/600/300/?image=25" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div><!-- FIN card -->
+      </div><!-- FIN container -->
     </div><!-- FIN col -->
   </div><!-- FIN Row -->  
 </div><!-- FIN Container -->
